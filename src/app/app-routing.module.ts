316adc4cf1af2c363e2ac9918e690a7dc8f48fae
Joshua Immordino <joshimmor@Gmail.com>
2022-03-26 23:21:37 -0400

    ncaa delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NCAAComponent } from './ncaa/ncaa.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 
